538cb13069c840fe1f98e5539aff6176305fa2c6
EitorBrandao <30750907+EitorBrandao@users.noreply.github.com>
2019-11-07 20:29:33 -0300

    Add files via upload

--- src/boundary/TelaPrincipal.java
+++ src/boundary/TelaPrincipal.java
@@ -1,28 +1,29 @@
 package boundary;
 
-import control.ControlTelas;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class TelaPrincipal extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
+	@Override
+	public void start(Stage stage) throws Exception {
 
-        stage.setTitle ("Aventura Interativa");
+		stage.setTitle("Aventura Interativa");
 
-        TelaLogin telaLogin = new TelaLogin();
-        TelaSave telaSave = new TelaSave ();
-        //TelaGame telaGame = new TelaGame ();
+		TelaLogin telaLogin = new TelaLogin();
+		TelaSave telaSave = new TelaSave();
+		TelaGame telaGame = new TelaGame ();
 
-        stage.setScene(telaLogin.gerarTela ());
-        stage.show();
-    }
+		stage.setScene(telaLogin.gerarTela());
+		stage.setScene(telaSave.gerarTela());
+		stage.setScene(telaGame.gerarTela()); 
+		stage.show();
+	}
 
-    public void tro
+//    public void tro
 
-    public static void main(String[] args){
+	public static void main(String[] args) {
 
-        launch(args);
-    }
+		launch(args);
+	}
 }
--- src/boundary/TelaSave.java
+++ src/boundary/TelaSave.java
@@ -4,33 +4,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 
 public class TelaSave implements Telas {
 
-    @Override
-    public Scene gerarTela() {
+	private BorderPane paneB0 = new BorderPane();
+	private GridPane paneG0 = new GridPane();
+	private Scene telaSave = new Scene(paneB0, 1000, 800);
 
-        BorderPane paneB0 = new BorderPane ();
-        GridPane paneG0 = new GridPane ();
+	private Button btnSlot1 = new Button("Slot 1");
+	private Button btnSlot2 = new Button("Slot 2");
+	private Button btnSlot3 = new Button("Slot 3");
 
-        Button btnSlot1 = new Button ("Slot 3");
-        Button btnSlot2 = new Button ("Slot 3");
-        Button btnSlot3 = new Button ("Slot 3");
+	@Override
+	public Scene gerarTela() {
 
-        btnSlot1.setPrefSize (100,50);
-        btnSlot2.setPrefSize (100,50);
-        btnSlot3.setPrefSize (100,50);
+		btnSlot1.setPrefSize(100, 50);
+		btnSlot2.setPrefSize(100, 50);
+		btnSlot3.setPrefSize(100, 50);
 
+		paneG0.add(btnSlot1, 1, 0);
+		paneG0.add(btnSlot2, 1, 1);
+		paneG0.add(btnSlot3, 1, 2);
 
-        paneG0.add (btnSlot1,1,0);
-        paneG0.add (btnSlot2,1,1);
-        paneG0.add (btnSlot3,1,2);
+		paneB0.setCenter(paneG0);
 
-        paneB0.setCenter (paneG0);
-
-        Scene telaSave = new Scene (paneB0,1000,800);
-        return telaSave;
-    }
+		return telaSave;
+	}
 
 }
